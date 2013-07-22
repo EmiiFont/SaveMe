8cd711123c16625f121f0f8ec1d40ee0403ab50d
EmiiFont <emifont01@gmail.com>
2013-07-22 10:18:51 -0400

    ddd

--- SaveMe/SaveMeProject/MyScene.cs
+++ SaveMe/SaveMeProject/MyScene.cs
@@ -14,7 +14,7 @@ namespace SaveMeProject
         {
             RenderManager.BackgroundColor = Color.CornflowerBlue;
 
-            //Insert code here
+            //Insert code her
         }
     }
 }
